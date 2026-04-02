ec375b51c7a4149d53f3475accb27cdad25fd7a9
Sashafelix <frank.haughton@gmail.com>
2026-04-02 17:53:11 +0200

    feat: add backend/app/core/config.py

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET_KEY: str
+    APP_VERSION: str = "unknown"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
